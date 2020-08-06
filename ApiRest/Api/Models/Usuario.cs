68fceab9b135496604e2aac0b00d67949ee48f6a
Marcio <marcio.pcmonzon@gmail.com>
2020-08-06 19:03:55 -0300

    api rest com bd em memoria

---
+++ ApiRest/Api/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ ApiRest/Api/Models/UsuariosCtx.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class UsuariosCtx : DbContext
+    {
+        public UsuariosCtx(DbContextOptions<UsuariosCtx> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
