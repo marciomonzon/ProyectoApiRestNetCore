fe0a2e2c746693baa0f2611b1cf068737ff42c94
Marcio <marcio.pcmonzon@gmail.com>
2020-08-12 18:32:21 -0300

    cursos controller and his model

--- ApiRest/Cursos/Controllers/CursosController.cs
+++ ApiRest/Cursos/Controllers/CursosController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cursos.Helper;
+using Cursos.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cursos.Controllers
 {
@@ -11,5 +14,55 @@ namespace Cursos.Controllers
     [ApiController]
     public class CursosController : ControllerBase
     {
+        private readonly CursosCTX _ctx;
+
+        public CursosController(CursosCTX ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Curso>> Get()
+        {
+            return await _ctx.Curso.ToListAsync();
+        }
+
+        [HttpGet("{id}")] 
+        public async Task<IActionResult> Get(int id)
+        {
+            var curso = await _ctx.Curso.FindAsync(id);
+
+            if (curso == null)
+            {
+                return NotFound(ErrorHelper.Response(404, $"Curso {id} no encontrado."));
+            }
+
+            return Ok(curso);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Curso curso)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ErrorHelper.GetModelStateErrors(ModelState));
+            }
+
+            if (await _ctx.Curso.Where(x => x.IdCurso == curso.IdCurso).AnyAsync())
+            {
+                return BadRequest(ErrorHelper.Response(400, $"El id del curso {curso.IdCurso} yá existe!"));
+            }
+
+            if (await _ctx.Curso.Where(x => x.Codigo == curso.Codigo).AnyAsync())
+            {
+                return BadRequest(ErrorHelper.Response(400, $"El código {curso.Codigo} yá existe!"));
+            }
+
+            curso.Estado = curso.Estado ?? true; // por default es true
+            _ctx.Curso.Add(curso);
+            await _ctx.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = curso.IdCurso }, curso);
+        }
     }
 }
--- ApiRest/Cursos/Models/Curso.cs
+++ ApiRest/Cursos/Models/Curso.cs
@@ -14,9 +14,13 @@ namespace Cursos.Models
         public int IdCurso { get; set; }
 
         [StringLength(10)]
+        [MaxLength(10, ErrorMessage = "El código debe ser máximo de 10 caracteres")]
+        [MinLength(2, ErrorMessage = "El código debe ser mínimo de 2 caracteres")]
         public string Codigo { get; set; }
 
         [StringLength(100)]
+        [MaxLength(100, ErrorMessage = "La descripcion debe ser máximo de 100 caracteres")]
+        [MinLength(5, ErrorMessage = "La descripcion debe ser mínimo de 5 caracteres")]
         public string Descripcion { get; set; }
 
         public bool? Estado { get; set; }
